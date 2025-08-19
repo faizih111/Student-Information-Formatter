2cf32681274457918e249f60c7bc127780bb217b
faizih111 <eng.faizan11@gmail.com>
2025-08-19 14:14:19 +0500

    Update and rename MiniProject_02.py to main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ quote = input("Enter your favorite quote: ")
 
 print("___Student Information Formatter___")
 
-print("Name          :", name)
-print("Initial       :", name[:1] + "." + name[10:])
-print("Course        :", f"{course} (Leangth: {len(course)})")
+print("Name:", name)
+print("Initial:", name[:1] + "." + name[10:])
+print("Course:", f"{course} (Leangth: {len(course)})")
 
 Quote = quote.replace("Weaknes", "Power")
 print("Favorite Quote:", Quote)
