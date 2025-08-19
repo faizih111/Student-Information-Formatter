a6b73f34469b8ba2e1820f62ce5341005ac216b5
faizih111 <eng.faizan11@gmail.com>
2025-08-19 14:01:22 +0500

    update code

--- MiniProject_02.py
+++ MiniProject_02.py
@@ -1,10 +1,13 @@
-name=input("Enter your name: ")
-course=input("Enter your course: ")
-quote=input("Enter your favorite quote: ")
+name = input("Enter your name: ")
+course = input("Enter your course: ")
+quote = input("Enter your favorite quote: ")
+
 print("___Student Information Formatter___")
-print("Name          :",name)
-print("Initial       :",name[:1]+"."+name[10:])
-print("Course        :",f"{course} (Leangth: {len(course)})")
-Quote=quote.replace("Weaknes","Power")
-print("Favorite Quote:",Quote)
-print("Reversed Name:",name[::-1])
+
+print("Name          :", name)
+print("Initial       :", name[:1] + "." + name[10:])
+print("Course        :", f"{course} (Leangth: {len(course)})")
+
+Quote = quote.replace("Weaknes", "Power")
+print("Favorite Quote:", Quote)
+print("Reversed Name:", name[::-1])
